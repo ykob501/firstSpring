ce965fde6595450789f921866c1877cda3c5ec40
ykob501 <42543742+ykob501@users.noreply.github.com>
2021-01-19 14:31:48 +0900

    Initial commit

---
+++ src/main/java/com/lee/book/springboot/web/HelloController.java
@@ -0,0 +1,24 @@
+package com.lee.book.springboot.web;
+
+
+import com.lee.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hi hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+}
+
+
